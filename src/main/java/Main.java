712fe57624203d62cd1e9a3fe34d89df19be11a7
Farhan <farhansarang7@gmail.com>
2025-03-24 22:21:22 -0400

    Implemented type builtins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
 
-        boolean exitShell = false;
+    private static boolean exitShell = false;
+
+    private static Set<String> BUILTINS = Set.of("exit", "echo", "type");
+
+    public static void main(String[] args) throws Exception {
 
         while (!exitShell) {
             System.out.print("$ ");
@@ -11,23 +14,33 @@ public class Main {
             String input = scanner.nextLine();
             input.trim();
 
-
             boolean validInput = false;
-            String command = input.split("\\s+")[0];
+            String[] slicedInput = input.toLowerCase().split("\\s+");
 
-            switch (command) {
+            switch (slicedInput[0]) {
                 case "echo":
                     System.out.println(input.substring(input.indexOf(" ") + 1));
                     validInput = true;
                     break;
+
                 case "exit":
-                    String[] slicedInput = input.split("\\s+");
                     switch (slicedInput.length > 1 ? slicedInput[1] : "") {
                         case "0":
                             validInput = true;
                             exitShell = true;
                             break;
                     }
+                    break;
+                    
+                case "type":
+                    if (slicedInput.length > 1 && BUILTINS.contains(slicedInput[1])) {
+                        System.out.println(slicedInput[1] + " is a shell builtin");
+                    } else if (slicedInput.length > 1) {
+                        System.out.println(slicedInput[1] + ": not found");
+                    } else {
+                        System.out.println("Expected Arguments!");
+                    }
+                    validInput = true;
                     break;
             }
 
