7f14c7ed42339959bd4a4db50289dc0f30fd8474
Farhan <farhansarang7@gmail.com>
2025-03-24 20:46:50 -0400

    The exit builtin.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
 
         while (true) {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            input = input.trim();
+
+            if (input.startsWith("exit")) {
+                String code = input.split("\\s+")[1];
+                switch (code) {
+                    case "1":
+                        break;
+                    default:
+                        System.out.println("Invalid Status Code!");
+                }
+                break;
+            }
             System.out.println(input + ": command not found");
         }
     }
